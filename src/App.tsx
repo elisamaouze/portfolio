fc10e67a3a92aeea4d77233cc19983b71efb664a
elisamaouze <elisa.maouze@gmail.com>
2025-10-05 01:47:34 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Header } from './components/Header';
+import { Hero } from './components/Hero';
+import { Projects } from './components/Projects';
+import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <Hero />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
